69eb8ac22d6ee9b03ed5dc80573ce6a782885ce8
thauandev <thauandepaula@icloud.com>
2020-09-02 14:25:20 -0300

    Estilizando Dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,34 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Container, Title } from './styles';
+import logoImg from '../../assets/logo.svg';
+
+import { Container, Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <Title>Dashboard</Title>;
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Dashboard</Title>
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars1.githubusercontent.com/u/36938649?s=460&u=f209fb7c748a839bb9677c76994ce3096291e64b&v=4"
+            alt="teste"
+          />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>Easy peasy Form</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
